54b046a45ab5772ec33b57cccb19c7a535cc94bb
Youcef <youcefcopy@gmail.com>
2026-02-14 16:33:13 +0100

    min & max logic is working wothout emits

--- src/components/Exercice1/CounterContainer.vue
+++ src/components/Exercice1/CounterContainer.vue
@@ -5,13 +5,23 @@ import { ref } from 'vue'
 // define the increemnt function
 
 const count = ref(0)
+const max = ref(10)
+const min = ref(0)
 const increment = () => {
   // handle the max here
+  if (count.value >= max.value) {
+    console.log('you are out of bounds!')
+    return
+  }
   count.value++
 }
 
 const descrement = () => {
   // handel the min here
+  if (count.value <= min.value) {
+    console.log('too small dude')
+    return
+  }
   count.value--
 }
 
@@ -20,14 +30,14 @@ const reset = () => {
 }
 
 // handle the max count
-const handleMax = (limit) => {
-  if (limit < 0) {
-    console.log('cannot go beyond 0')
-  }
-  if (limit > count.value) {
-    console.log('you have surpassed the limit')
-  }
-}
+// const handleMax = (limit) => {
+//   if (limit < 0) {
+//     console.log('cannot go beyond 0')
+//   }
+//   if (limit > count.value) {
+//     console.log('you have surpassed the limit')
+//   }
+// }
 </script>
 
 <template>
