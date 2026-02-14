2f77573a77ea9cec3697afdfc7cfd4d24aa0f5e5
Youcef <youcefcopy@gmail.com>
2026-02-14 13:18:08 +0100

    2nd emit is working

--- src/components/Exercice1/CounterContainer.vue
+++ src/components/Exercice1/CounterContainer.vue
@@ -8,10 +8,14 @@ const count = ref(0)
 const increment = () => {
   count.value++
 }
+
+const descrement = () => {
+  count.value--
+}
 </script>
 
 <template>
-  <MyCounter :count="count" @up="increment" />
+  <MyCounter :count="count" @up="increment" @down="descrement" />
 </template>
 
 <style scoped></style>
