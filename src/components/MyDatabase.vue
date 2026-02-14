b98840de43763275ccc5bc28d87078bcb1d41748
Youcef <youcefcopy@gmail.com>
2026-02-14 09:53:29 +0100

    settig up the components

---
+++ src/components/MyDatabase.vue
@@ -0,0 +1,14 @@
+<script setup>
+// this is the parent/container component that handles the children.
+
+import { ref } from 'vue'
+const database = ref([])
+
+console.log(database)
+</script>
+
+<template>
+  <h1>You did it!</h1>
+</template>
+
+<style scoped></style>
