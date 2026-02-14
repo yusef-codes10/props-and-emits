71916c86efbfdb6f4b4301fec58223eb1382d522
Youcef <youcefcopy@gmail.com>
2026-02-14 09:57:38 +0100

    the database array has been added

--- src/components/MyDatabase.vue
+++ src/components/MyDatabase.vue
@@ -2,7 +2,15 @@
 // this is the parent/container component that handles the children.
 
 import { ref } from 'vue'
-const database = ref([])
+const database = ref([
+  { firstName: 'Peter', lastName: 'Parker', department: 'Marketing', age: 23 },
+  { firstName: 'Mary Jane', lastName: 'watson', department: 'Marketing', age: 22 },
+  { firstName: 'Harry', lastName: 'Osborn', department: 'Scince', age: 23 },
+  { firstName: 'Gwen', lastName: 'Stacy', department: 'Science', age: 23 },
+  { firstName: 'Eddie', lastName: 'Brock', department: 'Science', age: 25 },
+  { firstName: 'Liz', lastName: 'Alan', department: 'Bilology', age: 23 },
+  { firstName: 'Flash', lastName: 'Tompson', department: 'biology', age: 24 },
+])
 
 console.log(database)
 </script>
