fbcfea5b79f3c5d332a9a0988475430ac8176c84
Youcef <youcefcopy@gmail.com>
2026-02-14 12:29:10 +0100

    preparing for exercice 1

---
+++ src/components/Exercice1/CounterContainer.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>counter container</template>
+
+<style scoped></style>
---
+++ src/components/Exercice1/MyCounter.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>This is the counter itself</template>
+
+<style scoped></style>
